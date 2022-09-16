0bb2ec1a7e751081fd2d1a95b889a576cea079a7
Veniamin Alekseev <alksven@gmail.com>
2022-09-16 15:50:27 +0300

    inition

---
+++ Independent_practice/15_module/1_task/main.py
@@ -0,0 +1,18 @@
+nums_list = []
+
+N = int(input('Enter number of numbers: '))
+for _ in range(N):
+    num = int(input('Next number: '))
+    nums_list.append(num)
+
+maximum = nums_list[0]
+minimum = nums_list[0]
+
+for i in nums_list:
+    if maximum < i:
+        maximum = i
+    if minimum > i:
+        minimum = i
+
+print('The maximum number in the list:', maximum)
+print('The minimum number in the list:', minimum)
