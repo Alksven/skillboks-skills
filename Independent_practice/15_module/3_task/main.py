fad21b4b487979c57e0d0a440d830fe04a94b917
Veniamin Alekseev <alksven@gmail.com>
2022-09-17 17:13:54 +0300

    finished task 3

--- Independent_practice/15_module/3_task/main.py
+++ Independent_practice/15_module/3_task/main.py
@@ -1,7 +1,40 @@
-count_dog = 6
-dog_list = [6, 5, 7, 23, 45, 12, 9]
-dog_lict_new = []
-for i_count in range(count_dog):
-    if dog_list[i_count] > dog_list[i_count + 1]:
-        dog_lict_new.append(dog_list[i_count])
-print(dog_lict_new)
+# count_dog = 6
+# dog_list = [6, 5, 7, 23, 45, 12, 9]
+# dog_list_help = sorted(dog_list)
+# dog_list_new = []
+# for count in range(count_dog, 0, -1):
+#     dog_list_new.append(dog_list_help[count])
+# print(dog_list_new)
+
+nums_list = []
+
+N = int(input('Кол-во чисел в списке: '))
+
+for _ in range(N):
+    num = int(input('Очередное число: '))
+    nums_list.append(num)
+
+if nums_list:
+    maximum = nums_list[0]
+    minimum = nums_list[0]
+
+    minimum_index = 0
+    maximum_index = 0
+    for index, i in enumerate(nums_list):
+
+        if maximum < i:
+            maximum = i
+            maximum_index = index
+
+        if minimum > i:
+            minimum = i
+            minimum_index = index
+
+    print('Максимальное число в списке:', maximum)
+    print('Минимальное число в списке:', minimum)
+
+    print(nums_list)
+    nums_list[minimum_index], nums_list[maximum_index] = nums_list[maximum_index], nums_list[minimum_index]
+    print(nums_list)
+else:
+    print('В списке нету чисел')
