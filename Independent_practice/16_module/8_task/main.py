71c7cbc920bb0a522b11472eb5eb5ccffb9c3741
Veniamin Alekseev <alksven@gmail.com>
2022-09-29 18:39:47 +0300

    fixed task 8

--- Independent_practice/16_module/8_task/main.py
+++ Independent_practice/16_module/8_task/main.py
@@ -1,13 +1,13 @@
-num_of_participants = int(input('Number of participants: '))
-num_of_team_members = int(input('Number of team members: '))
+num_of_part = int(input('Number of participants: '))
+num_of_t_mem = int(input('Number of team members: '))
 participants_list = []
 count = 1
 
-if num_of_participants // num_of_team_members != 0:
+if num_of_part // num_of_t_mem != 0:
     print('Error')
 
-for _ in range(num_of_participants // num_of_team_members):
-    participants_list.append(list(range(count, count + num_of_team_members)))
-    count += num_of_team_members
+for _ in range(num_of_part // num_of_t_mem):
+    participants_list.append(list(range(count, count + num_of_t_mem)))
+    count += num_of_t_mem
 
 print('General list of participants:', participants_list)
