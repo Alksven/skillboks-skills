764213d149a983a1964048a9c171853dfc829249
Veniamin Alekseev <alksven@gmail.com>
2022-10-16 13:33:28 +0300

    finished task #10, fixed task #8

--- skillbox/Python_Basic/Module17/08_entertainment/main.py
+++ skillbox/Python_Basic/Module17/08_entertainment/main.py
@@ -1,24 +1,17 @@
 import random
 
 stick = int(input('Количество палок: '))
-stick_list = [1 for _ in range(stick)]
+stick_list = ['|' for _ in range(stick)]
 
 throw = int(input('Количество бросков: '))
 
 throw_list_down = [random.randint(1, stick) for _ in range(throw)]
 throw_list_up = [random.randint(throw_list_down[x], stick) for x in range(throw)]
 
-answer = ''
 
 for a in range(throw):
     for x in range(throw_list_down[a] - 1, throw_list_up[a]):
-        stick_list[x] = 0
+        stick_list[x] = '.'
 
-for i in stick_list:
-    if i == 1:
-        answer += '|'
-    else:
-        answer += '.'
-
-print(answer)
+print(''.join(stick_list))
 
