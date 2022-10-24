dc95e0a79eb536b727ecc5d16b428feaa0923509
Veniamin Alekseev <alksven@gmail.com>
2022-10-24 14:56:11 +0300

    finished task 7

--- skillbox/Python_Basic/Module18/07_ip_adress_2/main.py
+++ skillbox/Python_Basic/Module18/07_ip_adress_2/main.py
@@ -1,26 +1,34 @@
+def function(help_variable):
+
+    if help_variable.isdigit() == False:
+        print(help_variable, '— это не целое число.')
+        exit()
+    elif int(help_variable) > 255:
+        print(help_variable, 'превышает 255.')
+        exit()
+    else:
+        return help_variable
+
+
 ip = input('Введите IP: ')
 new_ip = []
-a = ''
+help_variable = ''
+
 
 for i in ip:
     if i.isdigit() or i.isalpha():
-        a += i
+        help_variable += i
     else:
-        if a.isdigit() == False:
-            print(a, '— это не целое число.')
-            break
-        elif int(a) > 255:
-            print(a, 'превышает 255.')
-            break
+        a_list = function(help_variable)
+        new_ip.append(a_list)
+        help_variable = ''
+        if i != '.':
+            print('Адрес — это четыре числа, разделённые точками.')
+            exit()
         else:
-            new_ip.append(a)
-            a = ''
-            if i != '.':
-                print('Адрес — это четыре числа, разделённые точками.')
-                break
-            else:
-                new_ip.append(i)
-else:
-    print('IP-адрес корректен.')
+            new_ip.append(i)
 
-print(new_ip)
+a_list = function(help_variable)
+
+
+print('IP-адрес корректен.')
