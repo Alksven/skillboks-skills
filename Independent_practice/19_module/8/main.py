99dea288a2d1f787bfec347b19e43a2ddc1ff68f
Veniamin Alekseev <alksven@gmail.com>
2022-10-30 19:22:39 +0300

    started tasks 2, 1

---
+++ Independent_practice/19_module/8/main.py
@@ -0,0 +1,14 @@
+players_dict = {
+
+    1: {'name': 'Vanya', 'team': 'A', 'status': 'Rest'},
+    2: {'name': 'Lena', 'team': 'B', 'status': 'Training'},
+    3: {'name': 'Maxim', 'team': 'C', 'status': 'Travel'},
+    4: {'name': 'Egor', 'team': 'C', 'status': 'Rest'},
+    5: {'name': 'Andrei', 'team': 'A', 'status': 'Training'},
+    6: {'name': 'Sasha', 'team': 'A', 'status': 'Rest'},
+    7: {'name': 'Alina', 'team': 'B', 'status': 'Rest'},
+    8: {'name': 'Masha', 'team': 'C', 'status': 'Travel'}
+
+}
+
+for i in players_dict[]['name']
