c0a7d2969e1fcf9d8074de8a38cf0662d4e623ed
Veniamin Alekseev <alksven@gmail.com>
2022-11-03 17:50:39 +0300

    finishe task 1

---
+++ skillbox/Python_Basic/Module19/01_songs_2/main.py
@@ -0,0 +1,20 @@
+violator_songs = {
+    'World in My Eyes': 4.86,
+    'Sweetest Perfection': 4.43,
+    'Personal Jesus': 4.56,
+    'Halo': 4.9,
+    'Waiting for the Night': 6.07,
+    'Enjoy the Silence': 4.20,
+    'Policy of Truth': 4.76,
+    'Blue Dress': 4.29,
+    'Clean': 5.83
+}
+
+num_songs = int(input('Сколько песен выбрать? '))
+name_songs = {input(f'Название {i + 1} песни: ') for i in range(num_songs)}
+time_songs = 0
+
+for i in name_songs:
+    time_songs += violator_songs[i]
+
+print('Общее время звучания песен: {0} минуты'.format(time_songs))
