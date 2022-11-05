673671ea4f9e0ac8ced47ee3cc38d9d80e0e1eef
Veniamin Alekseev <alksven@gmail.com>
2022-11-05 19:55:55 +0300

    finished task 2

--- skillbox/Python_Basic/Module19/02_geography/main.py
+++ skillbox/Python_Basic/Module19/02_geography/main.py
@@ -1,25 +1,21 @@
-
 dict_country = dict()
 
 num_of_country = int(input('Количество стран: '))
 
 for count in range(num_of_country):
     country_and_city = input(f'{count + 1} страна: ').split()
-    dict_country[country_and_city[0]] = country_and_city[1:]
-
+    for i in range(1, len(country_and_city)):
+        dict_country[country_and_city[i]] = country_and_city[0]
 
 for search_city in range(1, 4):
     city = input(f'Введите город {search_city}: ')
-    for i in dict_country.values():
-        if city == i:
-            print('asdf')
+    if city in dict_country:
+        print(f'Город {city} расположен в стране {dict_country[city]}')
+    else:
+        print(f'По городу {city} данных нет.')
 
 
-print(dict_country)
 
-# city = input('Первый город: ')
-# print()
 
-# Россия Москва Петербург Новгород
-# Германия Берлин Лейпциг Мюнхен
+
 
