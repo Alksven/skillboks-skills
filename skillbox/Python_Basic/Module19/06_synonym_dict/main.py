8071d72b138d5da1d83f48653685dfc87ec1a6e7
Veniamin Alekseev <alksven@gmail.com>
2022-11-09 15:39:02 +0300

    finished task 6

--- skillbox/Python_Basic/Module19/06_synonym_dict/main.py
+++ skillbox/Python_Basic/Module19/06_synonym_dict/main.py
@@ -3,14 +3,14 @@ num_of_word = int(input('Введите количество пар слов: ')
 dict_word = dict()
 for i in range(num_of_word):
     word = input(f'{i + 1} пара: ').lower().split()
-    dict_word[word[0]] = word[2]
+    dict_word[word[2]] = word[0]
 print(dict_word)
 
 search = False
 while search != True:
-    search_word = input('Введите слово: ')
-    if search_word in dict_word.values():
-        print('Синоним:', dict_word.keys([search_word]))
+    search_word = input('Введите слово: ').lower()
+    if search_word in dict_word:
+        print('Синоним:', dict_word[search_word].capitalize())
         search = True
     else:
         print('Такого слова в словаре нет.')
