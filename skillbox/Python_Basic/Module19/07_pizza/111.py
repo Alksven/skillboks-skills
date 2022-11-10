1692dc6a96f5b848fdfdfe94b8148ffdec855098
Veniamin Alekseev <alksven@gmail.com>
2022-11-10 18:51:21 +0300

    I'm still doing task 7.

--- skillbox/Python_Basic/Module19/07_pizza/111.py
+++ skillbox/Python_Basic/Module19/07_pizza/111.py
@@ -1,16 +1,24 @@
 num_of_order = 2 #int(input('Введите количество заказов: '))
 order_dict = dict()
+client = []
 
-count = 0
+count = 1
 for num_order in range(num_of_order):
-    add_order = 'Иванов Мясная 3'.split()
+    add_order = input(f'{num_order + 1} заказ ').split()
+    if num_order == 0:
+        order_dict[add_order[0]] = {'pizza': add_order[1], 'amount': int(add_order[2])}
+        continue
+    if add_order[0] in order_dict and order_dict[add_order[0]]['pizza'] != add_order[1]:
+        order_dict[add_order[0]] = {'pizza': add_order[1], 'amount': int(add_order[2])}
+    if add_order[0] in order_dict and order_dict[add_order[0]]['pizza'] == add_order[1]:
+        order_dict[add_order[0]]['amount'] += int(add_order[2])
+    else:
+        order_dict[add_order[0]] = {'pizza': add_order[1], 'amount': int(add_order[2])}
 
-    for count in order_dict.values():
-        if count['client'] == add_order[0] and count['pizza'] == add_order[1]:
-            order_dict[0]['amount'] += int(add_order[2])
-        else:
-            count += 1
-            order_dict[count] = {'client': add_order[0], 'pizza': add_order[1], 'amount': int(add_order[2])}
+
 
 print(order_dict)
 
+# Иванов Пепперони 1
+# Петров Де-Люкс 2
+# Иванов Мясная 3
--- skillbox/Python_Basic/Module19/07_pizza/main.py
+++ skillbox/Python_Basic/Module19/07_pizza/main.py
@@ -1,17 +1,21 @@
 num_of_order = 2 #int(input('Введите количество заказов: '))
 order_dict = dict()
-client = ''
-pizza = ''
-count = 0
+client = []
+
+count = 1
 for num_order in range(num_of_order):
     add_order = input(f'{num_order + 1} заказ ').split()
-    client = add_order[0]
-    pizza = add_order[1]
-    if client == order_dict[0]['client'] and pizza == order_dict[0]['pizza']:
-        order_dict[0]['amount'] += int(add_order[2])
+    if num_order == 0:
+        order_dict[count] = {'client': add_order[0], 'pizza': add_order[1], 'amount': int(add_order[2])}
+        continue
+    client = add_order[1:]
+    for count in range(count):
+    if add_order[1] == client[0] and add_order[2] == client[1]:
+        order_dict[1]['amount'] += int(add_order[2])
     else:
         count += 1
         order_dict[count] = {'client': add_order[0], 'pizza': add_order[1], 'amount': int(add_order[2])}
+        client = []
 
 print(order_dict)
 
