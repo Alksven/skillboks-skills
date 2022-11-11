d51d8c7c90c9392822f3338b069c3c2ffd800e0c
Veniamin Alekseev <alksven@gmail.com>
2022-11-11 17:14:12 +0300

    finished task 7

--- skillbox/Python_Basic/Module19/07_pizza/main.py
+++ skillbox/Python_Basic/Module19/07_pizza/main.py
@@ -1,24 +1,18 @@
-num_of_order = 2 #int(input('Введите количество заказов: '))
+num_of_order = int(input('Введите количество заказов: '))
 order_dict = dict()
-client = []
 
-count = 1
 for num_order in range(num_of_order):
-    add_order = input(f'{num_order + 1} заказ ').split()
-    if num_order == 0:
-        order_dict[count] = {'client': add_order[0], 'pizza': add_order[1], 'amount': int(add_order[2])}
-        continue
-    client = add_order[1:]
-    for count in range(count):
-    if add_order[1] == client[0] and add_order[2] == client[1]:
-        order_dict[1]['amount'] += int(add_order[2])
+    name, pizza, amount = input(f'{num_order + 1} заказ ').split()
+    amount = int(amount)
+    if name not in order_dict:
+        order_dict[name] = {pizza: amount}
     else:
-        count += 1
-        order_dict[count] = {'client': add_order[0], 'pizza': add_order[1], 'amount': int(add_order[2])}
-        client = []
+        if pizza not in order_dict[name]:
+            order_dict[name] |= {pizza: amount}
+        else:
+            order_dict[name][pizza] += amount
 
-print(order_dict)
-
-# Иванов Пепперони 1
-# Петров Де-Люкс 2
-# Иванов Мясная 3
+for name, order in sorted(order_dict.items()):
+    print(f'{name}:')
+    for pizza, amount in sorted(order.items()):
+        print('    ', pizza, amount)
