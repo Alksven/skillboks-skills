613623adea5e3d1134a9eda64231d3fc654ad1fe
Veniamin Alekseev <alksven@gmail.com>
2022-12-01 23:03:28 +0300

    continue task 9

--- skillbox/Python_Basic/Module20/09_competition_protocol/main.py
+++ skillbox/Python_Basic/Module20/09_competition_protocol/main.py
@@ -1,26 +1,34 @@
+def function():
+    for i in pro_dict:
+        if i[0] in win_dice and i[1] < win_dice[i[0]]:
+            continue
+        else:
+            win_dice[i[0]] = i[1]
+
+
+def f_winner():
+    for i in range(3):
+
+
+
 num_of_pro = int(input('Сколько записей вносится в протокол? '))
 pro_dict = dict()
-win_sort = set()
+win_dice = dict()
 
 print('Записи (результат и имя):')
 for i_pro in range(num_of_pro):
     add_pro = input(f'{i_pro + 1}-я запись: ').split()
-    for i in pro_dict:
-        if int(add_pro[1]) > i
-    pro_dict[add_pro[1]] = int(add_pro[0])
-    print(pro_dict)
+    pro_dict[(add_pro[1], int(add_pro[0]))] = i_pro
 
-win_sort = list(win_sort)
-win_sort.sort(reverse=True)
+function()
+f_winner()
 
-win_list = [n for i in win_sort[0:3] for n in pro_dict if i in n]
+print(win_dice)
 
-for i_win, win in enumerate(win_list):
-    print(f'{i_win + 1}-е место. {win[1]} ({win[0]})')
-
-
-
-
+# winners = sorted(pro_dict.items(), key=lambda item: item[1], reverse=True)
+#
+# for i in range(3):
+#     print(f'{i + 1}-е место. {winners[i][0]} ({winners[i][1]})')
 
 # 69485 Jack
 # 95715 qwerty
