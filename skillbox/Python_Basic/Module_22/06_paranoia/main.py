34e97649d245bbba88c0334ebbf60d3f149380b7
Ven <alksven@gmail.com>
2023-01-06 20:19:04 +0300

    continue task 6

--- skillbox/Python_Basic/Module_22/06_paranoia/main.py
+++ skillbox/Python_Basic/Module_22/06_paranoia/main.py
@@ -1,15 +1,27 @@
-import os
+def encryption():
 
-letters = ['abcdefghijklmnopqrstuvwxyz']
-text = open('text.txt', 'r')
+    text = open('text.txt', 'r')
+    ready_file = open('cipher_text.txt', 'a')
 
-for n_str, word in enumerate(text):
-    for i in word:
-        print(letters[i])
+    for step, word in enumerate(text):
+        for l in word:
+            if l.isalpha():
+                if l.isupper():
+                    ready_file.write(letters_upper[letters_upper.index(l) + step + 1])
+                else:
+                    ready_file.write(letters_lower[letters_lower.index(l) + step + 1])
+            else:
+                ready_file.write(l)
+
+def length():
 
 
 
 
+letters_lower = 'abcdefghijklmnopqrstuvwxyz'
+letters_upper = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
 
 
 
+
+encryption()
