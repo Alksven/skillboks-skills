dd156803052ac53e49b92ae94671d5a4daba4d87
Ven <alksven@gmail.com>
2023-01-08 19:12:44 +0300

    finised task 7

--- skillbox/Python_Basic/Module_22/07_tournament/main.py
+++ skillbox/Python_Basic/Module_22/07_tournament/main.py
@@ -1,21 +1,23 @@
 first_tour = open('first_tour.txt', 'r')
+all_people = list()
+winner_list = dict()
 
-list_tour = list()
-winner = dict()
-for i, str in enumerate(first_tour):
-    list_tour.append(str.split())
+for i, str_1 in enumerate(first_tour):
+    all_people.append(str_1.split())
 
-for i_2 in range(1, len(list_tour)):
-    if int(list_tour[i_2][2]) > int(list_tour[0][0]):
-        winner[i_2] = [list_tour[i_2][1][0] + '.', list_tour[i_2][0], int(list_tour[i_2][2])]
+for i_2 in range(1, len(all_people)):
+    if int(all_people[i_2][2]) > int(all_people[0][0]):
+        winner_list[i_2] = [all_people[i_2][1][0] + '.', all_people[i_2][0], int(all_people[i_2][2])]       # составляем список в нужнопм порядке значений
 
 
 second_tour = open('second_tour.txt', 'a')
+second_tour.write(str(len(winner_list)) + '\n')
 
-ready_list = '{}'.format(len(winner))
-p = 1
-for i_3, nb in enumerate(sorted(winner.values(), key=lambda x: x[2], reverse=True)):
+for i_3, nb in enumerate(sorted(winner_list.values(), key=lambda x: x[2], reverse=True)):        #отсортированный список по местам
+    second_tour.write(str(i_3 + 1) + ') ' + " ".join(map(str, nb)) + '\n')      #записываем файл 2 тура
 
+first_tour.close()
+second_tour.close()
 
 
 
