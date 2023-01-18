6b78af5b13efa9ce37d027d2655de235aed21043
Ven <alksven@gmail.com>
2023-01-18 17:29:17 +0300

    finished task 5

--- skillbox/Python_Basic/Module_23/05_text_calc/main.py
+++ skillbox/Python_Basic/Module_23/05_text_calc/main.py
@@ -1,29 +1,38 @@
-def fun(task):
-    try:
-        a, sym, b = task.split()
-        if len(sym) != 1:
-            raise BaseException
-    except BaseException:
-        print(f'Обнаружена ошибка в строке: {i[0:len(i) - 1]} Хотите исправить?')
-    else:
-        if sym == '+':
-        if sym == '-':
-        if sym == '*':
-        if sym == '/':
-        if sym == '//':
-        if sym == '**':
-
+def fun(a, sym, b):
+    a = int(a)
+    b = int(b)
+    if sym == '+':
+        return a + b
+    elif sym == '-':
+        return a - b
+    elif sym == '*':
+        return a * b
+    elif sym == '/':
+        return a / b
+    elif sym == '//':
+        return a // b
+    elif sym == '**':
+        return a ** b
 
 
 summ = 0
 with open('calc.txt', 'r') as cals:
 
-    for i in cals:
-         answer_task = fun(i)
-         summ += answer_task
+    for i, task in enumerate(cals):
+        try:
+            a, sym, b = task.split()
+            answer_task = fun(a, sym, b)
+            summ += answer_task
 
+        except TypeError:
+            print(f'Обнаружена ошибка в строке: {task[0:len(task) - 1]} Хотите исправить?')
+            select = input().lower()
+            if select == 'да':
+                edit = input('Введите исправленную строку: ').split()
+                answer_task = fun(edit[0], edit[1], edit[2])
+                summ += answer_task
 
-
+print('Сумма результатов:', summ)
 
 
 
