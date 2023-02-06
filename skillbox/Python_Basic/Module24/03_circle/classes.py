4cd2835219f7a3b420cc2bfe14816ed47adafa95
Ven <alksven@gmail.com>
2023-02-06 20:36:57 +0300

    finished task 3

--- skillbox/Python_Basic/Module24/03_circle/classes.py
+++ skillbox/Python_Basic/Module24/03_circle/classes.py
@@ -1,16 +1,36 @@
+import math
 class Circle:
-    circles_list = []
+    PI = math.pi
 
-    def __init__(self, num_circle, x=0, y=0, r=6):
+    def __init__(self, num_circle, x, y, r):
         self.num_circle = num_circle
         self.x = x
         self.y = y
         self.r = r
-        self.circles_list.append([self.num_circle, self.x, self.y, self.r])
-        print('{} created'.format(num_circle))
-
+        print(self.num_circle, 'Создан круг')
 
     def circle_area(self):
-        s = 3.14 * (self.r ** 2)
-        print(self.r)
+        s = self.PI * (self.r ** 2)
         print('Площадь круга = {}'.format(s))
+
+    def circle_perimeter(self):
+        c = 2 * self.PI * self.r
+        print('Периметр круга = {}'.format(c))
+
+    def enlarge_circle(self):
+        add_size = int(input('Во сколько раз увеличить круг? '))
+        self.r = self.r * add_size
+        print('Новый радиус круга = {}'.format(self.r))
+
+    def intersections(self, old_x, old_y, old_r, num_old_circle):
+        hypotenuse = (abs(self.x - old_x) ** 2) + (abs(self.y - old_y) ** 2)
+        summ_r = self.r + old_r
+        print(hypotenuse)
+        print(summ_r)
+        if summ_r >= math.sqrt(hypotenuse):
+            print('Круг пересекается с кругом {}'.format(num_old_circle))
+        else:
+            print('Круг не пересекается с кругом {}.'.format(num_old_circle))
+
+
+
--- skillbox/Python_Basic/Module24/03_circle/main.py
+++ skillbox/Python_Basic/Module24/03_circle/main.py
@@ -1,26 +1,41 @@
 from classes import Circle
 
-
 def add_circle():
-    print(f'Enter circle coordinates {count_circle}')
+    print(f'Введите параметры круга: {count_circle}')
     x_point = float(input("x = "))
     y_point = float(input("y = "))
     r_circle = float(input("R = "))
-    Circle(f'circle_{count_circle}', x_point, y_point, r_circle)
+    circle_dict[count_circle] = Circle(f'circle_{count_circle}', x_point, y_point, r_circle)
 
-action = '1. Найти площадь этого круга.\n2. Найти периметр этого круга.\n3. Увеличивать этот круг.\n' \
+
+action = '\n1. Найти площадь этого круга.\n2. Найти периметр этого круга.\n3. Увеличивать этот круг.\n' \
          '4. Пересекается ли тот круг с другими.\n5. Добавить еще один круг.\n6. Завершить программу.'
+
 count_circle = 1
-select_action = 0
-Circle(f'circle_{count_circle}')
-print(Circle.circles_list)
-while select_action != 6:
+circle_dict = dict()
+circle_dict[count_circle] = Circle(f'circle_{count_circle}', 0.0, 0.0, 2.0)
+
+while True:
     print(action)
     select_action = int(input('Выберите действие: '))
     if select_action == 1:
-        Circle(f'circle_{count_circle}').circle_area()
+        circle_dict[count_circle].circle_area()
+    if select_action == 2:
+        circle_dict[count_circle].circle_perimeter()
+    if select_action == 3:
+        circle_dict[count_circle].enlarge_circle()
+    if select_action == 4:
+        if len(circle_dict) > 1:
+            for i in range(1, len(circle_dict)):
+                circle_dict[count_circle].intersections(circle_dict[i].x, circle_dict[i].y, circle_dict[i].r, circle_dict[i].num_circle)
+        else:
+            print('Вы добавили всего один круг. Нельзя проверить пересечения.')
+
     if select_action == 5:
         count_circle += 1
         add_circle()
+    if select_action == 6:
+        print('Программа завершена')
+        break
 
 
