69c812f79c3d51a896aa83275d14c771a9c44962
Ven <alksven@gmail.com>
2023-02-11 17:10:28 +0300

    the task 4 is ready

--- skillbox/Python_Basic/Module24/04_fathers_mothers_kids/classes.py
+++ skillbox/Python_Basic/Module24/04_fathers_mothers_kids/classes.py
@@ -7,19 +7,32 @@ class Parent:
         self.children_list = list()
         Parent.parents_list.append(self)
 
-
     def tell_about_yourself(self):
-        print("My name is {} and I am {} years old. ".format(self.name, self.age))
-        print("I have {} children.".format(len(self.children_list)))
-        for i in range(len(self.children_list)):
-            print(self.children_list[i].name, self.children_list[i].age, 'years old.')
+        print("Меня зовут {}, мне {} лет. ".format(self.name, self.age))
+        if len(self.children_list) == 0:
+            print('У меня нет детей.')
+        else:
+            print("Мои дети:")
+            for i in range(len(self.children_list)):
+                print(self.children_list[i].name, self.children_list[i].age, 'лет.')
 
+    def comfort_children(self, child):
+        action = input('{} успокоить ребенка?'.format(self.name)).lower()
+        if action == 'да':
+            child.calm = True
+        else:
+            while action != 'да':
+                action = input('{} орет! Успокоить? '.format(child.name)).lower()
+            child.calm = True
 
-    def comfort_children(self):
-        pass
-
-    def feeding_children(self):
-        pass
+    def feeding_children(self, child):
+        action = input('{} покормить ребенка? '.format(self.name)).lower()
+        if action == 'да':
+            child.hungry = True
+        else:
+            while action != 'да':
+                action = input('{} орет! Накормить? '.format(child.name)).lower()
+            child.hungry = True
 
 
 
@@ -31,10 +44,15 @@ class Child:
     def __init__(self, name, age):
         self.name = name
         self.age = int(age)
+        self.parent = []
         Child.children.append(self)
 
     def state_calm(self):
-        pass
+        print('{} начал(а) вредничать.'.format(self.name))
+        self.calm = False
+        self.parent[0].comfort_children(self)
 
     def state_hungry(self):
-        pass
+        print('{} проголодался.'.format(self.name))
+        self.calm = False
+        self.parent[0].feeding_children(self)
--- skillbox/Python_Basic/Module24/04_fathers_mothers_kids/main.py
+++ skillbox/Python_Basic/Module24/04_fathers_mothers_kids/main.py
@@ -2,19 +2,47 @@ from classes import Parent
 from classes import Child
 import random
 
-number_of_parents = int(input('Number of parents? '))
+def age_check(parent, count):
+    name, age = input("Введите имя и возраст {}го ребенка: ".format(count)).split()
+    while int(parent) - int(age) < 16:
+        age = int(input("Возраст слишком большой, пожалуйста, введите правильный возраст: "))
+    return name, age
+
+
+number_of_parents = int(input('Количество родителей? '))
 for _ in range(number_of_parents):
-    name_parent, age_parent = input("Enter parent's name and age: ").split()
+    name_parent, age_parent = input("\nВведите имя и возраст родителя: ").split()
     Parent(name_parent, age_parent)
+    have_many_children = int(input('Сколько детей у этого родителя? '))
 
-    have_many_children = int(input('How many children does {} have? '.format(name_parent)).lower())
     if have_many_children > 0:
-        for _ in range(have_many_children):
-            name_child, age_child = input("Enter child's name and age: ").split()
+        for count_children in range(have_many_children):
+            name_child, age_child = age_check(Parent.parents_list[-1].age, count_children + 1)
             Child(name_child, age_child)
-            if Parent.parents_list[-1].age - int(age_child) > 16:
-                Parent.parents_list[-1].children_list.append(Child.children[-1])
-            else:
-                print("the age difference is too small.")
-                continue
+            Parent.parents_list[-1].children_list.append(Child.children[-1])
+            Child.children[-1].parent.append(Parent.parents_list[-1])
 
+
+
+while True:
+    print()
+    for i, person in enumerate(Parent.parents_list):
+        print(i + 1, '=', person.name)
+    choose_parent = int(input('Выберите родителя: '))
+    choice = int(input('1 = Показать информацию о родителе.\n2 = Перейти к воспитанию детей.\n3 = Вернуться к списку родителей.\n'))
+    if choice == 3:
+        continue
+    elif choice == 1:
+        Parent.parents_list[choose_parent - 1].tell_about_yourself()
+    elif choice == 2:
+        if len(Parent.parents_list[choose_parent - 1].children_list) > 0:
+            choose_child = random.randint(1, len(Parent.parents_list[choose_parent - 1].children_list))
+            choice_of_behavior = random.randint(1, 2)
+            if choice_of_behavior == 1:
+                Parent.parents_list[choose_parent - 1].children_list[choose_child - 1].state_calm()
+            else:
+                Parent.parents_list[choose_parent - 1].children_list[choose_child - 1].state_hungry()
+        else:
+            print('Нет детей')
+    else:
+        print('Вы ввели не правильную команду')
