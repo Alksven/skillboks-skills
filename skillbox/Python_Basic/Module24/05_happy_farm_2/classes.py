d4801e02cd0fc9bcbd68dd6bb11c71560dbfe15b
Ven <alksven@gmail.com>
2023-02-18 17:37:45 +0300

    finised task 5

--- skillbox/Python_Basic/Module24/05_happy_farm_2/classes.py
+++ skillbox/Python_Basic/Module24/05_happy_farm_2/classes.py
@@ -1,34 +1,18 @@
-import random
-
-
 class Gardener:
 
     def __init__(self, name, garden):
         self.name = name
-        self.gardener_state = True
         self.job = garden
-
-    def drink_gardener(self):
-        state = random.randint(0, 1)
-        if state == 0:
-            self.gardener_state = False
-        else:
-            self.gardener_state = True
-        self.care()
+        self.harvest_1 = []
 
     def care(self):
-        choice_potato = random.randint(0, len(self.job.potato))
-        if self.gardener_state == False:
-            print('{} сегодня выпил и упал на картошку {} и повредил ее.'.format(
-                self.name,
-                self.job.potato[choice_potato].index))
-            Potato.step_back(self.job.potato[choice_potato])
-        else:
-            print('{} сегодня трезвый. Он полил картошку {}'.format(
-                self.name,
-                self.job.potato[choice_potato].index))
-            Potato.grow(self.job.potato[choice_potato])
+        print('{} полил картошку.\n'.format(self.name))
+        Garden.grow_all(self.job)
 
+    def harvest(self):
+        for i in self.job.potato:
+            print('{} собрал картошку {}'.format(self.name, i.index))
+            self.harvest_1.append(i.index)
 
 
 class Potato:
@@ -37,7 +21,6 @@ class Potato:
         1: 'Росток',
         2: 'Зелёная',
         3: 'Зрелая',
-        4: 'Сгнила'
     }
 
     def __init__(self, index):
@@ -49,16 +32,14 @@ class Potato:
             self.state += 1
         self.print_state()
 
-
-
-    def step_back(self):
-        self.state -= 1
-
-
     def print_state(self):
         print('Картошка {} сейчас {}'.format(self.index, Potato.states[self.state]
-        ))
+                                             ))
 
+    def is_ripe(self):
+        if self.state == 3:
+            return True
+        return False
 
 
 class Garden:
@@ -67,16 +48,13 @@ class Garden:
         self.potato = [Potato(index) for index in range(1, count_potato + 1)]
 
     def grow_all(self):
-        print('Картошка прорастает')
+        print('\nКартошка прорастает')
         for i_potato in self.potato:
             i_potato.grow()
 
-
-    def delete(self):
-        if self.potato[].state == 4:
-            print('WARNING: Картошка {} {} и пропала'.format(self.index, self.states[self.state]))
-        elif i.state == 0:
-            print('WARNING: Садовник уничтожил картошку {}'.format(i.index))
-            self.potato.remove(i)
-
-
+    def are_all_ripe(self):
+        if not all([i_potato.is_ripe() for i_potato in self.potato]):
+            print('\nКартошка еще не созрела\n')
+        else:
+            print("Картошка вся созрела. Можно собирать\n")
+            return True
--- skillbox/Python_Basic/Module24/05_happy_farm_2/main.py
+++ skillbox/Python_Basic/Module24/05_happy_farm_2/main.py
@@ -7,18 +7,15 @@ count_potato = int(input('Сколько картошки посадим?'))
 my_garden = Garden(count_potato)
 gardener = Gardener(name_gar, my_garden)
 
-while True:
+for i in range(3):
     my_garden.grow_all()
-    a = int(input('Что делаем? 1 продолжить 2 собрать урожай'))
-    if a == 1:
-        gardener.drink_gardener()
-
-
-
-
-
-
-
-
-
-
+    if my_garden.are_all_ripe():
+        gardener.harvest()
+        break
+    else:
+        care = random.randint(0, 1)
+        if care == 1:
+            gardener.care()
+            if my_garden.are_all_ripe():
+                gardener.harvest()
+                break
