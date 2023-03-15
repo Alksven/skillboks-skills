2aa01e93bc2f3647f987d7510a1c74773f46e773
Ven <alksven@gmail.com>
2023-03-15 17:48:40 +0300

    did task 7

--- skillbox/Python_Basic/Module25/07_stack/classes.py
+++ skillbox/Python_Basic/Module25/07_stack/classes.py
@@ -1,28 +1,46 @@
 class Stak:
     list_tasks = dict()
 
-    def __init__(self, task):
-        self.sort(task)
-
-
-
-    def sort(self, task):
-        if task[1] in self.list_tasks:
-            self.list_tasks[task[1]] += ', ' + task[0]
+    def sort(self, task, priority):
+        if priority in self.list_tasks:
+            self.list_tasks[priority].append(task)
         else:
-            self.list_tasks[task[1]] = task[0]
+            self.list_tasks[priority] = [task]
 
-        print(self.list_tasks)
-
-
+    def delete_task(self, task):
+        if len(self.list_tasks[task[1]]) == 1:
+            self.list_tasks[task[1]].remove(task[0])
+            del self.list_tasks[task[1]]
+        else:
+            self.list_tasks[task[1]].remove(task[0])
 
 
 class TaskManager:
 
+    def __init__(self):
+        self.stak = Stak()
+        self.all_tasks = []
+        self.completed_tasks = []
 
     def new_task(self, task, priority):
-        task_add = [task, priority]
-        Stak(task_add)
+        self.all_tasks.append([task, priority])
+        self.stak.sort(task, priority)
 
+    def delete_task(self):
+        print(f'\nЗадача "{self.all_tasks[-1][0]}" удалена.\n')
+        self.completed_tasks.append(self.all_tasks[-1])
+        self.stak.delete_task(self.all_tasks[-1])
+        self.all_tasks.pop()
 
+    def info_completed_tasks(self):
+        print('\nЗавершенный список задач:')
+        for num, task in enumerate(self.completed_tasks):
+            print(f'{num + 1}. {task[0]}')
+            if num + 1 == len(self.completed_tasks):
+                print()
 
+    def __str__(self):
+        print('Текущий список задач:')
+        for i in sorted(self.stak.list_tasks):
+            print(f"{i} - {', '.join(str(x) for x in self.stak.list_tasks[i])}")
+        return ''
--- skillbox/Python_Basic/Module25/07_stack/main.py
+++ skillbox/Python_Basic/Module25/07_stack/main.py
@@ -3,9 +3,22 @@ from classes import TaskManager
 manager = TaskManager()
 
 manager.new_task("сделать уборку", 4)
-manager.new_task("помыть посуду", 4)
 manager.new_task("отдохнуть", 1)
 manager.new_task("поесть", 2)
 manager.new_task("сдать ДЗ", 2)
+manager.new_task("помыть посуду", 4)
 
-# print(manager)
+while True:
+    print(manager)
+    action = int(input('Выберите действие:\n1 = Добавить новую задачу.\n2 = Удалить задачу.'
+                       '\n3 = Посмотреть список выполненных задач.\n4 = Выход из программы.\nВвод...'))
+    if action == 1:
+        task = input('Введите новую задачу: ')
+        priority = int(input(f'Введите приоритет задачи: '))
+        manager.new_task(task, priority)
+    if action == 2:
+        manager.delete_task()
+    if action == 3:
+        manager.info_completed_tasks()
+    if action == 4:
+        break
