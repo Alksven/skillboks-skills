123a81c572286b85ed4a02b18dc7954dcced7fbb
Ven <alksven@gmail.com>
2023-03-17 14:57:53 +0300

    edited task 3

--- skillbox/Python_Basic/Module25/03_my_dict/classes.py
+++ skillbox/Python_Basic/Module25/03_my_dict/classes.py
@@ -1,21 +1,9 @@
-class Dict:
 
-    def __init__(self):
-        self.dict = dict()
-
-    def set_element(self, key, value):
-        self.dict[key] = value
-
-    def get_key(self, key):
-        return self.dict.get(key)
-
-
-class MyDict(Dict):
-
+class MyDict(dict):
     def __init__(self):
         super().__init__()
 
-    def get_key(self, key):
-        if key in self.dict:
-            return f'Key value {key} = {self.dict[key]}.'
-        return self.dict.get(key, 0)
+    def get(self, key):
+        if key in self:
+            return self[key]
+        return 0
--- skillbox/Python_Basic/Module25/03_my_dict/main.py
+++ skillbox/Python_Basic/Module25/03_my_dict/main.py
@@ -1,16 +1,7 @@
-from classes import MyDict
+from classes2 import MyDict
 
 person = MyDict()
 
-action = 1
-while action != 0:
-    action = int(input('\n1 = Add/change element.\n2 = Check element.\n0 = Exit.\nEnter: '))
-    if action == 0:
-        break
-    if action == 1:
-        key, value = input('Enter the keys and its value: ').split()
-        person.set_element(key=key, value=value)
+person[2] = '34'
 
-    if action == 2:
-        key = input('Enter the key: ')
-        print(person.get_key(key))
+print(person.get(2))
