84b8ec20fb85b05a2448e1a96ea9ba416955f68c
Ven Alekseev <alksven@gmail.com>
2023-03-22 16:35:48 +0300

    did task 2 of module 26

--- skillbox/Python_Basic/Module26/02_files_path/main.py
+++ skillbox/Python_Basic/Module26/02_files_path/main.py
@@ -1,1 +1,17 @@
-# TODO здесь писать код
+import os
+
+def gen_files_path(directory):
+    for current_directory, subdirectories, files in os.walk(directory):
+        yield current_directory
+
+
+root_directory = os.path.abspath(os.sep)
+gen_files_path = gen_files_path(root_directory)
+
+search = input('Какйо каталог ищем? ')
+for path in gen_files_path:
+    if path.endswith(search):
+        print('Каталог найден.\nСписок файлов:')
+        path_2 = os.path.abspath(path)
+        for i_2 in os.listdir(path_2):
+            print(os.path.join(path_2, i_2))
