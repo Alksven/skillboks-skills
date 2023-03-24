fd506b66df2da45ac5d46256ff68e7db34ac2540
Ven Alekseev <alksven@gmail.com>
2023-03-24 19:54:40 +0300

    started task 4 of module 26

--- skillbox/Python_Basic/Module26/04_linked_list/main.py
+++ skillbox/Python_Basic/Module26/04_linked_list/main.py
@@ -1,1 +1,12 @@
-# TODO здесь писать код
+from classes import LinkedList
+
+
+my_list = LinkedList()
+my_list.append(10)
+my_list.append(20)
+my_list.append(30)
+print('Текущий список:', my_list)
+# print('Получение третьего элемента:', my_list.get(2))
+print('Удаление второго элемента.')
+my_list.remove(1)
+print('Новый список:', my_list)
