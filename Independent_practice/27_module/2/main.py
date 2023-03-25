7b77c39a7f0c841df6b754a6a52c056712c61372
Ven Alekseev <alksven@gmail.com>
2023-03-25 18:21:20 +0300

    did task 2 of module 27

---
+++ Independent_practice/27_module/2/main.py
@@ -0,0 +1,16 @@
+import time
+
+def timer(func, *args, **kwargs):
+    start_time = time.time()
+    result = func()
+    end_time = time.time()
+    work_time = round(end_time - start_time, 4)
+    print(f'Время работы программы: {work_time}')
+    return result
+
+
+def hard_func():
+    return [x ** 2 ** x for x in range(22)]
+
+
+print(timer(hard_func))
