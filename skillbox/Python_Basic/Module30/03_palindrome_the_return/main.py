8bbf0aa86232c9ace75d68e3b148036dda117c5d
Ven Alekseev <alksven@gmail.com>
2023-04-16 16:04:51 +0300

    did task 3 of m30

--- skillbox/Python_Basic/Module30/03_palindrome_the_return/main.py
+++ skillbox/Python_Basic/Module30/03_palindrome_the_return/main.py
@@ -1,1 +1,14 @@
-# TODO здесь писать код
+from collections import Counter
+
+
+def can_be_poly(text):
+    counter = Counter(text)
+    odd_count = sum(1 for count in counter.values() if count % 2 == 1)
+    if odd_count <= 1:
+        return True
+
+    return False
+
+
+print(can_be_poly('abcba'))
+print(can_be_poly('abbbc'))
