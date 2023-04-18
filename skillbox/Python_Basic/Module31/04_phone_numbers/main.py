0842ecd48a3cf2cae2241524c7b6578529164be2
Ven Alekseev <alksven@gmail.com>
2023-04-18 18:59:44 +0300

    did task 4

--- skillbox/Python_Basic/Module31/04_phone_numbers/main.py
+++ skillbox/Python_Basic/Module31/04_phone_numbers/main.py
@@ -1,1 +1,9 @@
-# TODO здесь писать код
+import re
+
+numbers = ['9999999999', '999999-999', '99999x9999']
+pattern_num = r'\b\d[89]\d{8}\b'
+for i, num in enumerate(numbers):
+    if re.search(pattern_num, num):
+        print(f'{i + 1} номер: всё в порядке')
+    else:
+        print(f'{i + 1} номер: не подходит')
