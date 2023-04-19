deeecaf6e9f07964ff6ebae3926019d2a4b289b8
Ven Alekseev <alksven@gmail.com>
2023-04-19 15:10:08 +0300

    did task 5

--- skillbox/Python_Basic/Module31/05_web_scrapping/main.py
+++ skillbox/Python_Basic/Module31/05_web_scrapping/main.py
@@ -1,1 +1,10 @@
-# TODO здесь писать код
+import requests
+from bs4 import BeautifulSoup
+
+
+website = requests.get('http://www.columbia.edu/~fdc/sample.html')
+html = website.content
+soup = BeautifulSoup(html, 'html.parser')
+
+texts = [p.text for p in soup.find_all('h3')]
+print(texts)
