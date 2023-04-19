b4c664c7d5e414f0969098e7b0249a804ae33f6c
Ven Alekseev <alksven@gmail.com>
2023-04-19 22:53:13 +0300

    did task 6

--- skillbox/Python_Basic/Module31/06_JSON_comparison/main.py
+++ skillbox/Python_Basic/Module31/06_JSON_comparison/main.py
@@ -1,1 +1,23 @@
-# TODO здесь писать код
+import json
+
+
+def find_diff(dict1, dict2):
+    for key, vel in dict1.items():
+        if dict1[key] != dict2[key] and key in diff_list:
+            diff[key] = dict1[key]
+        if isinstance(dict1[key], dict):
+            find_diff(dict1[key], dict2[key])
+
+
+diff_list = ['company_id', "services", "staff", "datetime"]
+
+diff = dict()
+
+with open('json_new.json', 'r') as json_new, open('json_old.json', 'r') as json_old:
+    data1 = json.load(json_new)
+    data2 = json.load(json_old)
+    find_diff(data1, data2)
+print(diff)
+
+with open('result.json', 'w') as result:
+    json.dump(diff, result)
